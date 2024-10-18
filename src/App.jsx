b2e6ecb0a0fa1598093c53cf9386728a4274be0e
Ashwin S Nambiar <76719333+Ashwin-S-Nambiar@users.noreply.github.com>
2024-10-18 09:03:52 +0530

    edited some formatting.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import "./App.css";
 import QuestionList from "./components/QuestionList/QuestionList";
 
-
 const App = () => {
 	const [gameStarted, setGameStarted] = useState(false);
 	const [showNoQuestionsError, setShowNoQuestionsError] = useState(false);
