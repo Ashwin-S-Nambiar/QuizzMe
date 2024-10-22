391969ce2d710921cc778e5662e1661cd455f41b
Ashwin S Nambiar <76719333+Ashwin-S-Nambiar@users.noreply.github.com>
2024-10-22 12:31:12 +0530

    Feature (#4)
    * added css edits for dark mode.
    
    * added dark mode implementation and improved transitions.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import QuestionList from "./components/QuestionList/QuestionList";
+import useLocalStorage from "use-local-storage";
+
+const SunIcon = () => (
+	<svg
+	  xmlns="http://www.w3.org/2000/svg"
+	  viewBox="0 0 24 24"
+	  fill="none"
+	  stroke="currentColor"
+	  strokeWidth="2"
+	  strokeLinecap="round"
+	  strokeLinejoin="round"
+	  className="feather feather-sun"
+	  width="24"
+	  height="24"
+	>
+	  <circle cx="12" cy="12" r="5"></circle>
+	  <line x1="12" y1="1" x2="12" y2="3"></line>
+	  <line x1="12" y1="21" x2="12" y2="23"></line>
+	  <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
+	  <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
+	  <line x1="1" y1="12" x2="3" y2="12"></line>
+	  <line x1="21" y1="12" x2="23" y2="12"></line>
+	  <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
+	  <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
+	</svg>
+  );
+  
+const MoonIcon = () => (
+	<svg
+	  xmlns="http://www.w3.org/2000/svg"
+	  viewBox="0 0 24 24"
+	  fill="none"
+	  stroke="currentColor"
+	  strokeWidth="2"
+	  strokeLinecap="round"
+	  strokeLinejoin="round"
+	  className="feather feather-moon"
+	  width="24"
+	  height="24"
+	>
+	  <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z"></path>
+	</svg>
+);  
 
 const App = () => {
 	const [gameStarted, setGameStarted] = useState(false);
@@ -14,6 +57,13 @@ const App = () => {
 		}
 	);
 	const [showFooter, setShowFooter] = useState(true);
+	const [darkTheme, setDarkTheme] = useLocalStorage("isDark",false);
+
+	useEffect(()=> {
+		if(darkTheme) {
+			document.body.classList.add("dark")
+		}
+	},[darkTheme])
 
 	const toggleFooter = () => {
 	  setShowFooter(!showFooter);
@@ -37,10 +87,18 @@ const App = () => {
 				[name]: value
 			}
 		});
-	}
+	};
+
+	const toggleTheme = () => {
+		document.body.classList.toggle("dark");
+		setDarkTheme(!darkTheme);
+	  };
 
 	return (
 		<main>
+			<button className="theme-toggle" onClick={toggleTheme}>
+        		{darkTheme ? <SunIcon /> : <MoonIcon />}
+      		</button>
 			
 			{
 				gameStarted
