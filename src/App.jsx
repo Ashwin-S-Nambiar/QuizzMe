276ab6b2ece6129135a9d8f09b359faa640be54d
Ashwin S Nambiar <76719333+Ashwin-S-Nambiar@users.noreply.github.com>
2024-10-22 15:20:46 +0530

    removed dark toggle from questionlist.jsx page.

--- src/App.jsx
+++ src/App.jsx
@@ -71,6 +71,7 @@ const App = () => {
 
 	const handleGameStart = () => {
 		toggleFooter();
+		document.getElementById("theme").style.display= "none";
 		setTimeout(() => {
 		  setGameStarted(prevState => !prevState);
 		}, 120);
@@ -96,7 +97,7 @@ const App = () => {
 
 	return (
 		<main>
-			<button className="theme-toggle" onClick={toggleTheme}>
+			<button aria-label="Theme Toggler" id="theme" className="theme-toggle" onClick={toggleTheme}>
         		{darkTheme ? <SunIcon /> : <MoonIcon />}
       		</button>
 			
